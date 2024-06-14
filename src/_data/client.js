b3ddce5a9dba32886465f40de813cd6675d67766
skelleyboy <dannyskelley@gmail.com>
2024-06-14 16:01:17 -0400

    aq
    a

--- src/_data/client.js
+++ src/_data/client.js
@@ -4,12 +4,12 @@ module.exports = {
     "phoneForTel": "313.801.7388",
     "phoneFormatted": "(313) 801-7388",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
+        "lineOne": "210 Rhode Island Street",
+        "lineTwo": "",
+        "city": "Highland Park",
+        "state": "MI",
         "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "mapLink": "https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d94266.72398017619!2d-83.1836836625901!3d42.40995579452259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x8824cdc288ba2611%3A0x64c31f2e8c195dd1!2sHighland%20Park%2C%20MI%2048203!3m2!1d42.409985299999995!2d-83.1012829!5e0!3m2!1sen!2sus!4v1718394264488!5m2!1sen!2sus"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
