f8a58d221ee7f9a38581bd95cf9dd8080dcce08b
skelleyboy <dannyskelley@gmail.com>
2024-06-21 14:13:50 -0400

    a
    a

--- src/_data/client.js
+++ src/_data/client.js
@@ -4,12 +4,12 @@ module.exports = {
     "phoneForTel": "(630) 241-0669",
     "phoneFormatted": "(630) 241-0669",
     "address": {
-        "lineOne": "test",
+        "lineOne": "1205 Maple Ave",
         "lineTwo": "",
-        "city": "Highland Park",
-        "state": "MI",
-        "zip": "80206",
-        "mapLink": "https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d94266.72398017619!2d-83.1836836625901!3d42.40995579452259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x8824cdc288ba2611%3A0x64c31f2e8c195dd1!2sHighland%20Park%2C%20MI%2048203!3m2!1d42.409985299999995!2d-83.1012829!5e0!3m2!1sen!2sus!4v1718394264488!5m2!1sen!2sus"
+        "city": "Lisle",
+        "state": "IL",
+        "zip": "60532",
+        "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2974.9660365507325!2d-88.0751324!3d41.785945600000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e5120cc03adc1%3A0x9cdb5fe6f3de2b89!2s1205%20Maple%20Ave%2C%20Lisle%2C%20IL%2060532!5e0!3m2!1sen!2sus!4v1718991268043!5m2!1sen!2sus"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
@@ -18,3 +18,5 @@ module.exports = {
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     "domain": "https://www.example.com"
 };
+
+
