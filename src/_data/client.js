8a9878048102bbd1913886c1df722ae9a9ef17bd
skelleyboy <dannyskelley@gmail.com>
2024-06-23 13:59:22 -0400

    a
    a

--- src/_data/client.js
+++ src/_data/client.js
@@ -11,8 +11,7 @@ module.exports = {
         "zip": "48212",
         "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d94172.13357567438!2d-83.20483236815633!3d42.47285837606464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d2685a8180f9%3A0xa454cbe1002071b6!2sKelly&#39;s%20Bar%20Hamtramck!5e0!3m2!1sen!2sus!4v1719164540545!5m2!1sen!2sus"
     },
-    
-    "socials": {
+ "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
     },
